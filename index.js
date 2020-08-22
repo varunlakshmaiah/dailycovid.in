1d34455dc332271f147b69422fcb89c234df2e65
Varun Lakshmaiah <varunlakshmaiah@yahoo.com>
2020-08-22 12:53:31 +0530

    ADD Global stats

--- index.js
+++ index.js
@@ -26,12 +26,34 @@ app.get('/api/covid/india',(req,res)=>{
     var config = {
         method: 'get',
         url: 'https://api.covid19india.org/data.json',
-        headers: { }
+        headers: {}
       };
       
       axios(config)
       .then(function (response) {
         let dataResponse = {status:"success",data:response.data}
+        res.json(dataResponse)
+      })
+      .catch(function (error) {       
+        let errorResponse = {status:"error","message":error.message}
+        res.json(errorResponse)             
+      });
+})
+
+
+app.get('/api/covid/stats',(req,res)=>{
+    var config = {
+        method: 'get',
+        url: 'https://thevirustracker.com/free-api?global=stats',
+        headers: {}
+      };
+      
+      axios(config)
+      .then(function (response) {
+        let data = response.data
+        data = data["results"][0]
+        delete data.source
+        let dataResponse = {status:"success",data}
         res.json(dataResponse)
       })
       .catch(function (error) {       
