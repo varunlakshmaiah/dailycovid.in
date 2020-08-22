cee9f90e72a72fe249111b9ddf9d6fa4a89d095c
Varun Lakshmaiah <varunlakshmaiah@yahoo.com>
2020-08-22 13:52:18 +0530

    ADD Moment and Req Res time

--- index.js
+++ index.js
@@ -2,17 +2,32 @@ const express = require('express')
 const axios = require('axios')
 const app = express()
 const PORT = 3000
+const moment = require('moment')
 
 const publicDir = __dirname+"/public/"
 const sendOpt = {
     root: publicDir
 }
 
+
+app.use(function (req, res, next) {
+    let clientIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+    let reqTime = moment();
+    console.log(`${clientIP}--> Request recieved`)
+    
+    res.on('finish', function(){
+        let resTime = moment();
+        let respDuration = moment.duration(resTime.diff(reqTime)).asSeconds();
+        console.log(`Responded to ${clientIP} in ${respDuration}s`);
+    });
+    next()
+  })
+
+  
 app.get('/', (req, res) => {
     res.sendFile("index.html",sendOpt,function(err){
       if(!err)
       {
-          console.log("Request completed");
       }
   })
 })
